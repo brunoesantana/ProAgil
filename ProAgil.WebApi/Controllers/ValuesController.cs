9db69c536efd8fcfc73c13441958d2b93b77ae11
bruno santana <bruno.santana@codeitsolutions.com.br>
2020-10-22 15:27:21 -0300

    primeiro commit

---
+++ ProAgil.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebApi.Data;
+using ProAgil.WebApi.Model;
+
+namespace ProAgil.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+
+                if(results != null && results.Any())
+                    return Ok(results);
+
+                return NotFound("Registro não encontrado.");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var result = await _context.Eventos.FirstOrDefaultAsync(f => f.EventoId == id);
+
+                if(result != null)
+                    return Ok(result);
+
+                return NotFound("Registro não encontrado.");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou.");
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
